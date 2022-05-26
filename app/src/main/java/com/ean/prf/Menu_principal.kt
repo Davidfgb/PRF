5a452c129722ab66b3c3df4b11fa9fe22a9b9ea7
Davidfgb <98990796+Davidfgb@users.noreply.github.com>
2022-05-26 17:47:49 -0500

    5.0

--- app/src/main/java/com/ean/prf/Menu_principal.kt
+++ app/src/main/java/com/ean/prf/Menu_principal.kt
@@ -1,7 +1,11 @@
 package com.ean.prf
 
+
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.firestore.ktx.firestore
@@ -16,8 +20,26 @@ class Menu_principal : AppCompatActivity() {
         setContentView(R.layout.activity_menu_principal)
 
         val boton_arroz = findViewById<Button>(R.id.bn_arroz_mp)
-        val id = findViewById<TextView>(R.id.editText_id_mp)
+        val boton_paisa= findViewById<Button>(R.id.bn_paisa_mp)
+        val boton_pasta= findViewById<Button>(R.id.bn_pasta_mp)
+        val boton_ajiaco= findViewById<Button>(R.id.bn_ajiaco_mp)
+        val boton_hamburguesa= findViewById<Button>(R.id.bn_hamburgesa_mp)
+        val email = findViewById<TextView>(R.id.editText_correo_am)
+        val boton_siguiente = findViewById<Button>(R.id.bn_siguiente_mp)
 
+        val id = findViewById<TextView>(R.id.editText_id_mp)
+        val comidas= arrayListOf<Pair<String,Int>>()
+        val precios= arrayListOf<Int>()
+        var valor_pedido = 0
+        var i = 0
+
+
+
+
+        boton_siguiente.setOnClickListener{
+            val intent = Intent(this,factura::class.java)
+            startActivity(intent)
+        }
 
 
 
@@ -29,12 +51,132 @@ class Menu_principal : AppCompatActivity() {
                 if (identi.isEmpty()) {
                     verifi.setText("No se ha ingresado un id")
                 } else {
-                    db.collection("comidas").document(id.text.toString()).set(
-                        hashMapOf(
-                            "palto principal" to "arroz con pollo",
-                        )
+                    val pedidos = hashMapOf(
+                        "principal" to comidas,
+                        "precios" to precios
                     )
+                    comidas.add(i,Pair("Arroz con Pollo",8900))
+                    precios.add(i,8900)
+                    i++
+                    valor_pedido+=8900
+                    db.collection("pedidos").document(id.text.toString())
+                        .set(pedidos)
+                        .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully written!") }
+                        .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error writing document", e) }
                     verifi.setText("Plato registrado")
+
+                }
+            }
+            catch (e:Exception){
+
+            }
+        }
+        boton_paisa.setOnClickListener {
+            val identi = id.text.toString()
+            var verifi = findViewById<TextView>(R.id.textView_verifi_mp)
+            try {
+                //verifi.setText("Pato Registrado")
+                if (identi.isEmpty()) {
+                    verifi.setText("No se ha ingresado un id")
+                } else {
+                    val pedidos = hashMapOf(
+                        "principal" to comidas,
+                        "precios" to precios,
+                    )
+                    comidas.add(i,Pair("Bandeja Paisa",10000))
+                    precios.add(i,10000)
+                    i++
+                    valor_pedido+=10000
+                    db.collection("pedidos").document(id.text.toString())
+                        .set(pedidos)
+                        .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully written!") }
+                        .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error writing document", e) }
+                    verifi.setText("Plato registrado")
+
+                }
+            }
+            catch (e:Exception){
+
+            }
+        }
+        boton_pasta.setOnClickListener {
+            val identi = id.text.toString()
+            var verifi = findViewById<TextView>(R.id.textView_verifi_mp)
+            try {
+                //verifi.setText("Pato Registrado")
+                if (identi.isEmpty()) {
+                    verifi.setText("No se ha ingresado un id")
+                } else {
+                    val pedidos = hashMapOf(
+                        "principal" to comidas,
+                        "precios" to precios,
+                    )
+                    comidas.add(i,Pair("Pasta",9500))
+                    precios.add(i,9500)
+                    i++
+                    valor_pedido+=9500
+                    db.collection("pedidos").document(id.text.toString())
+                        .set(pedidos)
+                        .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully written!") }
+                        .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error writing document", e) }
+                    verifi.setText("Plato registrado")
+
+                }
+            }
+            catch (e:Exception){
+
+            }
+        }
+        boton_ajiaco.setOnClickListener {
+            val identi = id.text.toString()
+            var verifi = findViewById<TextView>(R.id.textView_verifi_mp)
+            try {
+                //verifi.setText("Pato Registrado")
+                if (identi.isEmpty()) {
+                    verifi.setText("No se ha ingresado un id")
+                } else {
+                    val pedidos = hashMapOf(
+                        "principal" to comidas,
+                        "precios" to precios,
+                    )
+                    comidas.add(i,Pair("Ajiaco",7500))
+                    precios.add(i,7500)
+                    i++
+                    valor_pedido+=7500
+                    db.collection("pedidos").document(id.text.toString())
+                        .set(pedidos)
+                        .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully written!") }
+                        .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error writing document", e) }
+                    verifi.setText("Plato registrado")
+
+                }
+            }
+            catch (e:Exception){
+
+            }
+        }
+        boton_hamburguesa.setOnClickListener {
+            val identi = id.text.toString()
+            var verifi = findViewById<TextView>(R.id.textView_verifi_mp)
+            try {
+                //verifi.setText("Pato Registrado")
+                if (identi.isEmpty()) {
+                    verifi.setText("No se ha ingresado un id")
+                } else {
+                    val pedidos = hashMapOf(
+                        "principal" to comidas,
+                        "precios" to precios,
+                    )
+                    comidas.add(i,Pair("Hamburgueza",7500))
+                    precios.add(i,7500)
+                    i++
+                    valor_pedido+=7500
+                    db.collection("pedidos").document(id.text.toString())
+                        .set(pedidos)
+                        .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully written!") }
+                        .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error writing document", e) }
+                    verifi.setText("Plato registrado")
+
                 }
             }
             catch (e:Exception){
---
+++ app/src/main/java/com/ean/prf/factura.kt
@@ -0,0 +1,69 @@
+package com.ean.prf
+
+import Mundo.paswords_iguales
+import android.content.ContentValues
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.lang.Exception
+
+class factura : AppCompatActivity() {
+    val db = Firebase.firestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_factura)
+
+        val boton_finalizar = findViewById<Button>(R.id.bn_recuperar_fc)
+        var datos = findViewById<TextView>(R.id.textView_simm_fc)
+        val id = findViewById<TextView>(R.id.editText_nombre_fc)
+        val boton_confirmar = findViewById<TextView>(R.id.bn_confirmar_fc)
+        val boton_volver = findViewById<Button>(R.id.bn_regresar_fc)
+        val boton_pagar = findViewById<Button>(R.id.bn_recuperar_fc)
+        var mensaje= findViewById<TextView>(R.id.textView3_fc)
+        val identifi = id.text.toString()
+
+        boton_pagar.setOnClickListener {
+            mensaje.setText("su pedido ha sido registarado correctamente, ya estamos preparando su orden." +
+                    "porfavor vuelva al menu principal y espere su llamado")
+        }
+        boton_volver.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        boton_confirmar.setOnClickListener {
+            try {
+                    val docRef = db.collection("pedidos").document(id.text.toString())
+                    docRef.get()
+                        .addOnSuccessListener { document ->
+                            if (document != null) {
+                                Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
+                                datos.setText("${document.data}")
+                            } else {
+                                Log.d(ContentValues.TAG, "No such document")
+                            }
+                        }
+                        .addOnFailureListener { exception ->
+                            Log.d(ContentValues.TAG, "get failed with ", exception)
+                        }
+
+            }catch (e:Exception){
+                datos.setText("el usuario ingresado no exite, por favor verifique")
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
